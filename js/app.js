f80ca888d88cea92d22327b0a913c69385f0a204
JuanelCJ <juandavidrom@outlook.com>
2022-09-16 15:54:26 -0500

    Adding tabindex and focus event

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 let isMenuOpen = false
 
 const btnOpen = document.getElementById('btnOpen')
-btnOpen.addEventListener('click', ()=>handleBtnOpen())
-btnClose.addEventListener('click', ()=>handleBtnOpen())
+btnOpen.addEventListener('click', () => handleBtnOpen())
+btnClose.addEventListener('click', () => handleBtnOpen())
 const menu = document.getElementById('menu-display')
 
-const handleBtnOpen = () =>{
+const handleBtnOpen = () => {
     isMenuOpen = !isMenuOpen
-    if(!isMenuOpen){
+    if (!isMenuOpen) {
         menu.classList.remove('active')
-    }
-    else{
+    } else {
         menu.classList.add('active')
     }
 }
@@ -19,7 +18,34 @@ const handleBtnOpen = () =>{
 const search = document.querySelector('#search')
 const searchIcon = document.querySelector('#search #searchIcon')
 
-search.addEventListener('mouseover', ()=> searchIcon.classList.replace('fa-magnifying-glass','fa-xmark'))
-search.addEventListener('mouseout', ()=> searchIcon.classList.replace('fa-xmark','fa-magnifying-glass'))
+search.addEventListener('mouseover', () => searchIcon.classList.replace('fa-magnifying-glass', 'fa-xmark'))
+search.addEventListener('mouseout', () => searchIcon.classList.replace('fa-xmark', 'fa-magnifying-glass'))
 searchIcon.style.transition = '0.4s'
 
+// const openKeyBar = (e) =>{
+//     if (e.keyCode === 32){
+//         console.log(e)
+//     }
+//     else{
+//         console.log('Not working');
+//     }
+// }
+
+// openKeyBar()
+
+
+
+const focusSearch = search.addEventListener('focus', function (element) {
+    let content = document.querySelector('.dropdown-content.search')
+    document.addEventListener("keydown", function (event) {
+        if (event.key === 'Enter') {
+            content.style.display = 'block'
+        }
+        else if (event.key === '1') {
+            content.style.display = 'none'
+        }
+    })
+})
+
+
+document.getElementsByTagName('li')[0].focus();
